2b1214422bfbd39e546ee20d99f0b8759dbe70f1
Lucas Isaac Alves Galvão Mathias <lucasisaacbr@hotmail.com>
2017-11-22 21:11:07 -0200

    factory

--- server/helpers/dripFactory.js
+++ server/helpers/dripFactory.js
@@ -6,16 +6,6 @@
   const getDrip = require("getdrip-api")(process.env.DRIPAPITOKEN, process.env.DRIPACCOUNTID);
 
   module.exports = {
-    "getNomeComprador": function (comprador) {
-      return new Promise(function (resolve, reject) {
-        if(comprador) {
-          let result = comprador.split(" ")[0];
-          return resolve(result);
-        } else {
-          return reject("No name passed in");
-        }
-      });
-    },
     "validateKeys": function (chaveUnica, chaveProduto) {
 
       if(chaveUnica !== monetizeConfig.monetizeKey) {
@@ -24,41 +14,6 @@
 
       return chaveProduto === monetizeConfig.productKey;
     },
-
-    "startWorkflow": function (email, workflowId, opts) {
-
-      const host = 'https://api.getdrip.com/v2';
-      const header = [
-        {
-          name: 'content-type',
-          value: 'application/x-www-form-urlencoded'
-        }
-      ];
-
-      return new Promise(function (resolve, reject) {
-        let reqBody = {email: email};
-        let optionals = ['user_id', 'time_zone', 'custom_fields', 'tags', 'prospect'];
-
-        optionals.map(function (o) {
-          if (opts.hasOwnProperty(o))
-            reqBody[o] = opts[o];
-        });
-
-        console.log(reqBody);
-        request.post({
-          url: host + '/' + accountId + '/workflows/' + workflowId + '/subscribers',
-          auth: {'user': this.apiToken},
-          json: true,
-          header: this.header,
-          body: {subscribers: [reqBody]}
-        }, function (err, res, body) {
-          if (err) {
-            return reject(err);
-          } else {}
-        })
-      })
-    },
-
     "aguardandoPagamentoBoleto": function (data) {
       return new Promise(function (resolve, reject) {
 
@@ -66,24 +21,12 @@
           return reject("Data object is empty");
         }
 
-        let statusVenda = data["venda[status]"];
-        let usrEmail = data["comprador[email]"];
-        let formaPag = data["venda[formaPagamento]"];
-        let codigoProduto = data["produto[codigo]"];
         let flwUp_id = dripConfig.dripFollowUpID;
-        let urlBoleto = data["venda[linkBoleto]"] || "google";
-        let nome = data["comprador[nome]"] || " ";
+        let usrEmail = data["comprador[email]"] || "";
+        let urlBoleto = data["venda[linkBoleto]"] || 0;
+        let nome = data["comprador[nome]"].split(" ")[0] || " ";
 
-        console.log("A", statusVenda);
-        console.log("B", usrEmail);
-        console.log("C", formaPag);
-        console.log("D", codigoProduto);
-        console.log("E", flwUp_id);
-        console.log("F", urlBoleto);
-        console.log("G", nome);
 
-        if (! (statusVenda === 'Aguardando pagamento' && formaPag === 'Boleto' && codigoProduto === '39806')) {
-          console.log("AQ")
           getDrip.startOnAWorkflow(usrEmail, flwUp_id, {
             'custom_fields': {
               'linkBoleto': urlBoleto,
@@ -91,16 +34,77 @@
             }
           }, function (err, res, cb) {
             if(err){
+              console.log(err);
               reject(err);
             }
             resolve(cb);
           })
-        } else {
-          console.log("WAT ")
-        }
-
       });
+    },
+    "carrinhoAbandonado": function(data) {
+
+      let abandoned_id = process.env.DRIPABANDONEDID;
+      let usrEmail = data["comprador[email]"] || "";
+      let nome = data["comprador[nome]"].split(" ")[0] || " ";
+
+      return new Promise(function (resolve, reject) {
+        getDrip.startOnAWorkflow(usrEmail, abandoned_id, {
+          'custom_fields': {
+            "firstName": nome
+          }
+        }, function (err, res, body) {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(body);
+        });
+      })
+    },
+
+    "vendaCancelada": function (data) {
+      return new Promise(function (resolve, reject) {
+
+        let abandoned_id = process.env.DRIPABANDONEDID;
+        let usrEmail = data["comprador[email]"] || "";
+
+        getDrip.startOnAWorkflow(usrEmail, abandoned_id, {}, function (err, res, body) {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(body);
+        });
+      })
+    },
+    "vendaFinalizada": function (data) {
+      return new Promise(function (resolve, reject) {
+
+        let vsc_id = process.env.DRIPVSCID;
+        let usrEmail = data["comprador[email]"] || "";
+
+        getDrip.startOnAWorkflow(usrEmail, vsc_id, {}, function (err, res, body) {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(body);
+        });
+
+      })
+    },
+
+    "removerWorkflow": function (data) {
+
+      let usrEmail = data["comprador[email]"] || "";
+
+      return new Promise(function (resolve, reject) {
+        getDrip.unsubscribeFromCampaigns(usrEmail, function (err, res, body) {
+          if (err) {
+           return reject(err)
+          }
+          return resolve(body);
+        });
+      }) 
     }
+
   }
 
 }());
--- server/routes/helpers/apiHelper.js
+++ server/routes/helpers/apiHelper.js
@@ -6,17 +6,59 @@
   module.exports = function (app) {
 
     app.post("/webhook", function(req, res){
-      let data = require("../../../public/monetizze.json");
+
+      let data = req.body;
+      let statusVenda = data["venda[status]"] || "";
+      let formaPag = data["venda[formaPagamento]"] || "";
+      let codigoProduto = data["produto[codigo]"] || 0;
+      let nome = data["comprador[nome]"].split(" ")[0] || " ";
+
       if (!api.validateKeys(data.chave_unica, data["produto[chave]"])) {
 
-        api.aguardandoPagamentoBoleto(data).then(function (response) {
-          res.status(200).send(response);
-        }).catch(function (err) {
-          res.status(500).send(err);
-        });
+        if(statusVenda !== 'Aguardando pagamento' && statusVenda !== 'Cancelada' && statusVenda !== 'Finalizada'){
+          api.carrinhoAbandonado(data).then(function (response) {
+            res.status(200).send(response);
+          }).catch(function (err) {
+            console.log(err);
+            res.status(300).send(err);
+          });
+        }
+
+        if (statusVenda === 'Aguardando pagamento' && formaPag === 'Boleto' && codigoProduto === '39806') {
+          api.aguardandoPagamentoBoleto(data).then(function (response) {
+            res.status(200).send(response);
+          }).catch(function (err) {
+            console.log(err);
+            res.status(300).send(err);
+          });
+        }
+
+        else if (statusVenda === 'Cancelada' && codigoProduto === '39806') {
+          api.vendaCancelada(data).then(function (response) {
+            res.status(200).send(response);
+          }).catch(function (err) {
+            console.log(err);
+            res.status(300).send(err);
+          });
+        }
+
+        else if (statusVenda === 'Finalizada' && codigoProduto === '39806') {
+          api.removerWorkflow(data).then(function (response) {
+            api.vendaFinalizada(data).then(function (finalRes) {
+              res.status(200).send(finalRes);
+            }).catch(function (err) {
+              res.status(500).send(err);
+            })
+          }).catch(function (err) {
+            res.status(500).send(err);
+          })
+        } else {
+          res.status(404).send("Algo errado verique as validações");
+        }
+
+      } else {
+        res.status(403).send("Chave Invalida");
       }
-
-
     });
   }
 
